9a1ca12938e64add5073b77b13eb64d129ab4238
Abdul Wasay <irfansoomro370@gmail.com>
2026-01-16 21:53:02 +0500

    nginx web application complete infra provisioning on azure by creating vm vnet subnet public_ip network_interface nsg and many more things

---
+++ web-app-infra/main.tf
@@ -0,0 +1,104 @@
+# block parameters {
+#     arguments
+# }
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource-group-name
+  location = var.region
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "tf-demo-vnet"
+  location            = var.region
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/25"]
+
+}
+
+resource "azurerm_subnet" "frontend" {
+  name                 = "frontend-subnet"
+  address_prefixes     = ["10.0.0.0/27"]
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = var.resource-group-name
+}
+
+resource "azurerm_network_security_group" "frontend-nsg" {
+  name                = "frontend-nsg"
+  location            = var.region
+  resource_group_name = var.resource-group-name
+
+  security_rule {
+    name                       = "nginx"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "ssh access"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "tf-frontend-public-ip"
+  resource_group_name = var.resource-group-name
+  location            = var.region
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "tf-demo-nic"
+  location            = var.region
+  resource_group_name = var.resource-group-name
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.frontend.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.frontend-nsg.id
+}
+
+resource "azurerm_linux_virtual_machine" "tf-demo" {
+  name = "tf-demo-vm"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  size = var.vm_size
+  admin_username = var.admin_username
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+  os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  admin_ssh_key {
+    username = var.admin_username
+    public_key = file("id_rsa.pub")
+  }
+  tags = {
+    environment = "staging"
+  }
+}
---
+++ web-app-infra/varriables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+    default = "centralindia"
+}
+
+variable "resource-group-name" {
+    default = "tf-demo-rg"
+}
+
+variable "vm_size" {
+    default = "Standard_B1s"
+}
+
+variable "admin_username" {
+    default = "azureuser"
+}
