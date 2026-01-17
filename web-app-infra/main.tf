d497d7e88a3f4bc366ebb07b42b7dfd97c9b69f6
Abdul Wasay <irfansoomro370@gmail.com>
2026-01-17 14:52:52 +0500

    resolved issues in main.tf

--- web-app-infra/main.tf
+++ web-app-infra/main.tf
@@ -19,13 +19,13 @@ resource "azurerm_subnet" "frontend" {
   name                 = "frontend-subnet"
   address_prefixes     = ["10.0.0.0/27"]
   virtual_network_name = azurerm_virtual_network.vnet.name
-  resource_group_name  = var.resource-group-name
+  resource_group_name  = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_network_security_group" "frontend-nsg" {
   name                = "frontend-nsg"
-  location            = var.region
-  resource_group_name = var.resource-group-name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
     name                       = "nginx"
@@ -39,7 +39,7 @@ resource "azurerm_network_security_group" "frontend-nsg" {
     destination_address_prefix = "*"
   }
   security_rule {
-    name                       = "ssh access"
+    name                       = "ssh-access"
     priority                   = 1000
     direction                  = "Inbound"
     access                     = "Allow"
@@ -53,19 +53,21 @@ resource "azurerm_network_security_group" "frontend-nsg" {
 
 resource "azurerm_public_ip" "public_ip" {
   name                = "tf-frontend-public-ip"
-  resource_group_name = var.resource-group-name
-  location            = var.region
-  allocation_method   = "Dynamic"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+  sku = "Standard"
 }
 
 resource "azurerm_network_interface" "nic" {
   name                = "tf-demo-nic"
-  location            = var.region
-  resource_group_name = var.resource-group-name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.frontend.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
 
@@ -76,12 +78,12 @@ resource "azurerm_network_interface_security_group_association" "example" {
 }
 
 resource "azurerm_linux_virtual_machine" "tf-demo" {
-  name = "tf-demo-vm"
-  location = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  name                  = "tf-demo-vm"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.nic.id]
-  size = var.vm_size
-  admin_username = var.admin_username
+  size                  = var.vm_size
+  admin_username        = var.admin_username
 
   source_image_reference {
     publisher = "Canonical"
@@ -90,12 +92,12 @@ resource "azurerm_linux_virtual_machine" "tf-demo" {
     version   = "latest"
   }
   os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
+    name                 = "myosdisk1"
+    caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
   admin_ssh_key {
-    username = var.admin_username
+    username   = var.admin_username
     public_key = file("id_rsa.pub")
   }
   tags = {
--- web-app-infra/varriables.tf
+++ web-app-infra/varriables.tf
@@ -1,15 +1,15 @@
 variable "region" {
-    default = "centralindia"
+  default = "centralindia"
 }
 
 variable "resource-group-name" {
-    default = "tf-demo-rg"
+  default = "tf-demo-rg"
 }
 
 variable "vm_size" {
-    default = "Standard_B1s"
+  default = "Standard_B1s"
 }
 
 variable "admin_username" {
-    default = "azureuser"
+  default = "azureuser"
 }
